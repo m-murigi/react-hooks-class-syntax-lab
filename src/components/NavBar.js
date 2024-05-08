8cbbfd2087b9b62ec3a20b8818151ca8b752d428
Emmanuel <zacmac807@gmail.com>
2024-05-08 19:52:29 +0300

    add file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
-import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+import React, {Component} from "react";
+class NavBar extends Component  {
+  render(){
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
+
+
+
+ 
